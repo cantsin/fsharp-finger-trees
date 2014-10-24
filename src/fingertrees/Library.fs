0f92b0068d3990bc2fcf3981104eed27e0812d65
James Tranovich <jtranovich@gmail.com>
2014-10-23 20:50:21 -0700

    Added Monoid.

--- src/fingertrees/Library.fs
+++ src/fingertrees/Library.fs
@@ -1,5 +1,7 @@
 namespace fingertrees
 
+open Monoid
+
 /// Documentation for my library
 ///
 /// ## Example
@@ -7,11 +9,15 @@ namespace fingertrees
 ///     let h = Library.hello 1
 ///     printfn "%d" h
 ///
-module Library = 
-  
+module Library =
+
   /// Returns 42
   ///
   /// ## Parameters
   ///  - `num` - whatever
   let hello num = 42
 
+  type SumMonoid() =
+    inherit IMonoid<int>()
+    override this.mempty = 0
+    override this.mappend x y = x + y
---
+++ src/fingertrees/Monoid.fs
@@ -0,0 +1,9 @@
+namespace fingertrees
+
+module Monoid =
+  [<AbstractClass>]
+  type IMonoid<'a>() =
+    abstract member mempty : 'a
+    abstract member mappend : 'a -> 'a -> 'a
+    abstract member mconcat : seq<'a> -> 'a
+    default this.mconcat arr = Seq.fold this.mappend this.mempty arr
