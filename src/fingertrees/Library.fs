625d8adc4397b594d416855fd6c90126627df687
James Tranovich <jtranovich@gmail.com>
2014-10-24 11:28:23 -0700

    Added main function.

--- src/fingertrees/Library.fs
+++ src/fingertrees/Library.fs
@@ -21,3 +21,14 @@ module Library =
     inherit IMonoid<int>()
     override this.mempty = 0
     override this.mappend x y = x + y
+
+  type ProductMonoid() =
+    inherit IMonoid<int>()
+    override this.mempty = 1
+    override this.mappend x y = x * y
+
+  [<EntryPoint>]
+  let main args =
+    printfn "Arguments passed to function : %A" args
+    // Return 0. This indicates success.
+    0
