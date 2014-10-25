af39f8b1eef0e6b5ca2e422da5f40251af412b7c
James Tranovich <jtranovich@gmail.com>
2014-10-24 20:14:56 -0700

    Fingertree data structure and sample tree.

--- src/fingertrees/Library.fs
+++ src/fingertrees/Library.fs
@@ -2,6 +2,27 @@ namespace Fingertrees
 
 open Monoid
 
+type Node<'a> =
+  | Branch2 of 'a * 'a
+  | Branch3 of 'a * 'a * 'a
+
+type Affix<'a> =
+  | One of 'a
+  | Two of 'a * 'a
+  | Three of 'a * 'a * 'a
+  | Four of 'a * 'a * 'a * 'a
+
+// TODO restrict V to monoid
+type Finger<'V, 'T> = {
+  annotation: 'V;
+  prefix: Affix<'T>;
+  content: FingerTree<'V, Node<'T>>;
+  suffix: Affix<'T>;
+} and FingerTree<'V, 'T> =
+  | Empty        // special case: empty tree
+  | Single of 'T // special case: tree with one element
+  | Digit of Finger<'V, 'T>
+
 /// Documentation for my library
 ///
 /// ## Example
@@ -11,14 +32,25 @@ open Monoid
 ///
 module Library =
 
-  /// Returns 42
-  ///
-  /// ## Parameters
-  ///  - `num` - whatever
-  let hello num = 42
+  // test an example tree by hand
+  let prefix1: Affix<char> = Two('t','h')
+  let prefix2: Affix<Node<char>> = Two(Branch2('i','s'),
+                                       Branch2('i','s'));
+  let suffix2: Affix<Node<char>> = Two(Branch3('n','o','t'),
+                                       Branch2('a','t'));
+  let suffix1: Affix<char> = Three('r','e','e');
+
+  let content1: Finger<int, Node<char>> = { annotation = 1;
+                                            prefix = prefix2;
+                                            content = Empty;
+                                            suffix = suffix2; };
+  let content2: Finger<int, char> = { annotation = 0;
+                                      prefix = prefix1;
+                                      content = Digit(content1);
+                                      suffix = suffix1 };
+  let test_tree: FingerTree<int, char> = Digit(content2);
 
   [<EntryPoint>]
   let main args =
-    printfn "Arguments passed to function : %A" args
-    // Return 0. This indicates success.
+    printfn "sample finger tree: %A" test_tree
     0
